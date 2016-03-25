3e9ff4a8a84b663292ea50fa6a3a5b73fd777bcc
linoso <linoso@gmail.com>
2016-03-25 18:23:50 -0400

    First version of  theysayaboutus.

---
+++ public_html/eng/they-say-about-us.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+
+<title>Tey say about us | Genoastirling</title>
+<meta name="description" content="List of useful sites and pages on the Stirling engine, related systems and materials." />
+<meta name="keywords" content="Stirling engine useful link, stirling engine links, engine materials link" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Robots" content="INDEX, FOLLOW" />
+<meta name="Revisit-after" content="7 Days" />
+
+<?php include("headers.php");?>
+
+<!-- assegnazione swf a div -->
+<script type="text/javascript">
+
+      $(document).ready(function() {
+				initMenu("<?php print $_SERVER['REQUEST_URI'] ?>");
+      });
+</script>
+
+</head>
+
+<body class="pagine">
+<?php include("cookie.php");?>
+		<div class="header_2">
+			<a href="index.php" ><img src="../imgs/logo.png" width="177" height="70" alt="Genoastirling Stirling Engines" /></a>
+            <a href="http://www.genoastirling.it/diconono-di-noi.php"><img src="../imgs/logo_italia_2.jpg" class="eng" width="26" height="26" alt="Genoastirling Italian" /></a>
+        </div>
+        
+	<div class="wrapper">        
+        
+        <div class="medium_wrapper">
+        	<?php include("menu.php");?>
+            <div class="contenuto_2">
+                <h1 class="titoletto">They say about us</h1>
+				<div class="titoletto">KTH School of Industrial Engineering and Management and Universidad Mayor de San Simon (UMSS)</div>
+                <ul class="utili">
+                    <br/>
+                    <li><b><a href="http://www.diva-portal.se/smash/get/diva2:735271/FULLTEXT01.pdf" target="_blank">Price Model of the Stirling Engine</a></b> - Johanna Maiorana</li><br/>
+                    <li><b><a href="http://www.diva-portal.org/smash/get/diva2:735277/FULLTEXT01.pdf" target="_blank">Implementation of a Stirling engine generation system for residential use in rural areas of Beni department of Bolivia</a></b> - Hugo Ljunggren Falk, Sandra Berg</li><br/>
+                    <li><b><a href="https://www.diva-portal.org/smash/get/diva2:798500/FULLTEXT01.pdf" target="_blank">Thermodynamic analysis of Stirling engine systems</a></b> - Joseph Adhemar Araoz Ramos</li><br/>
+                    <li><b><a href="https://www.researchgate.net/publication/274405970_Development_and_validation_of_a_thermodynamic_model_for_the_performance_analysis_of_a_gamma_Stirling_engine_prototype" target="_blank">DEVELOPMENT AND VALIDATION OF A THERMODYNAMIC MODEL FOR THE PERFORMANCE ANALYSIS OF A GAMMA STIRLING ENGINE PROTOTYPE</a></b> - Joseph A. Araoz, Evelyn Cardozo, Marianne Salomona, Lucio Alejob, Torsten H. Franssona</li><br/>
+                    <li><b><a href="https://www.researchgate.net/publication/283228749_Numerical_simulation_for_the_design_analysis_of_kinematic_Stirling_engines" target="_blank">NUMERICAL SIMULATION FOR THE DESIGNANALYSIS OFKINEMATICSTIRLING ENGINES</a></b> - Joseph A. Araoz, Marianne Salomon, Lucio Alejo, Torsten H. Fransson</li><br/>
+			    </ul>
+                <div class="titoletto">GMTS - Grupo de Máquinas y Motores Térmicos de Sevilla</div>
+                <ul class="utili">
+                    <br/>
+                    <li><b><a href="http://bibing.us.es/proyectos/abreproy/5335/" target="_blank">POTENCIAL DE MEJORA DEL REFRIGERADOR DEL MOTOR STIRLING GENOA 03 E IMPACTO SOBRE LAS PRESTACIONES DEL MISMO</a></b> - Pablo Utrilla Noriega</li><br/>
+                </ul>
+                <div class="titoletto">Proceedings of ASME Turbo Expo 2012</div>
+                <ul class="utili">
+                    <br/>
+                    <li><b><a href="http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.658.1449&rep=rep1&type=pdf" target="_blank">SMART POLYGENERATION GRID: A NEW EXPERIMENTAL FACILITY</a></b> - Mario L. Ferrari, Matteo Pascenti, Alberto Traverso, Massimo Rivarolo</li><br/>
+                </ul>
+                <div class="titoletto">9th International Conference on Heat Transfer, Fluid Mechanics and Thermodynamics</div>
+                <ul class="utili">
+                    <br/>
+                    <li><b><a href="http://repository.up.ac.za/bitstream/handle/2263/42996/mangion_challanges_2014.pdf?sequence=1" target="_blank">CHALLENGES IN DEVELOPING A SOLAR POWERED STIRLING ENGINE FOR DOMESTIC ELECTRICITY GENERATION</a></b> - R. Mangion, M. Muscat, T. Sant, J.Rizzo, R. Ghirlando, J. Cilia, J. Mizzi, S. Vural</li><br/>
+                </ul>
+                <div class="titoletto">University of Malta</div>
+                <ul class="utili">
+                    <br/>
+                    <li><b><a href="http://www.ibimapublishing.com/journals/IJREB/2014/926227/926227.html" target="_blank">Challenges in Developing a Domestic Solar Thermal Electricity System Using a Stirling Engine</a></b> - R. Mangion, M. Muscat, T. Sant, J. G. Rizzo, R. Ghirlando, J. Cilia, J. Mizzi and S. Vural</li><br/>
+                </ul>
+                <div class="titoletto">University of Genoa</div>
+                <ul class="utili">
+                    <br/>
+                    <li><b><a href="http://www.tpg.unige.it/TPG/download_folder/SustainableEnergy/Lesson%20XIX%20-%20Stirling%20engines.pdf" target="_blank">Sustainable Energy: Fuel Cells & Distributed Generation Systems</a></b> - Dr. Ing. Mario L. Ferrari</li><br/>
+                </ul>
+            </div>
+			<div class="clear"></div>
+		</div>
+
+	</div>
+<?php include("footer.php");?>
+    
+</body>
+</html>
